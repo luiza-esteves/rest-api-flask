30414537a53f65c7e6a2ce4871e85e8b5c9288da
Luíza Esteves <luizaesteves987@gmail.com>
2024-06-27 08:29:35 -0300

    fix: correcao de ordem e versao do mongoengine

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = [
+    {
+        "db": "users",
+        "host": "mongodb",
+        "port": 27017,
+        "username": "admin",
+        "password": "admin"
+    }
+]
 api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf= db.StringField(required=True, unique=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    email = db.EmailField(required=True)
+    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
     def get(self):
-        return {'message': 'user 1'}
+        return {'message': 'user 7'}
 
 
 class User(Resource):
@@ -15,7 +34,7 @@ class User(Resource):
         return {'message': 'teste'}
 
     def get(self, cpf):
-        return {'message': 'cpf'}
+        return {'msessage': 'cpf'}
 
 
 api.add_resource(Users, '/users')
