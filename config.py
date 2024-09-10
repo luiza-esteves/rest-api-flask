22f44ab611a6b35f347edb333ddab3ab45512cdb
Luíza Esteves <luizaesteves987@gmail.com>
2024-09-10 17:50:31 -0300

    fix: usando variavel de ambiente

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
+import os
+
 class DevConfig():
 
 
-    MONGODB_SETTINGS = [
-        {
-            "db": "users",
-            "host": "mongodb",
-            "username": "admin",
-            "password": "admin"
-        }
-    ]
+    MONGODB_SETTINGS = {
+        'db': os.getenv('MONGODB_DB'),
+        'host': os.getenv('MONGODB_HOST'),
+        'username': os.getenv('MONGODB_USERNAME'),
+        'password': os.getenv('MONGODB_PASSWORD')
+    }
